3ced3c75461e30aec5c93bdf9053704c555b4676
Nibeza kevin <kevin.nibeza@gmail.com>
2021-08-17 12:01:49 +0200

    Removed the search by author because it had a bug in the prgram

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -102,16 +102,14 @@ class PostsController extends Controller
      protected function getPosts()
      {
     $posts = Post::latest();
-    $user = User::all();
+
         if(request(['search']) ?? false)
         {
          $posts
     ->where('title', 'like', '%' . request('search') . '%')
     ->orWhere('body', 'like', '%' . request('search'). '%');
 
-      $user
-    ->where('username', 'like', '%' . request('search') . '%')
-    ->orWhere('name', 'like', '%' . request('search'). '%');
+
 
     return view('SearchPage',[
         'posts' => $posts->get()
